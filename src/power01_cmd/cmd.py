89210cb60122371a3dc291c8be194b4892bbbee3
sooj1n <sujin19371@gmail.com>
2024-07-16 18:38:36 +0900

    cmd

---
+++ src/power01_cmd/cmd.py
@@ -0,0 +1,13 @@
+from power01_plus.plus import plus
+from power01_min.min import minus
+from power01_mul.mul import mul
+from power01_div.div import div
+
+
+def call():
+    plus()
+    minus()
+    mul()
+    div()
+
+    print('success!!')
